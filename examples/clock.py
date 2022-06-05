2d76dff225e5c980f57ca61e5a986da51f2ad946
Yasushi Itoh <i2y@icloud.com>
2022-06-05 22:45:53 +0900

    Add clock.py as an example

---
+++ examples/clock.py
@@ -0,0 +1,26 @@
+import threading
+import time
+from datetime import datetime
+
+from cattt.core import App, Component, State, Text
+from cattt.frame import Frame
+
+
+class Clock(Component):
+    def __init__(self):
+        super().__init__()
+        self._state = State(datetime.now().time().strftime("%X"))
+        self._thread = threading.Thread(target=self.run, args=())
+        self._thread.daemon = True
+        self._thread.start()
+
+    def run(self):
+        while True:
+            time.sleep(1)
+            self._state.set(datetime.now().time().strftime("%X"))
+
+    def view(self):
+        return Text(self._state)
+
+
+App(Frame("Counter", 600, 200), Clock()).run()
